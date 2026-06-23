cca878edc54a531940346c817b162cf7832de605
Breduck <110724253+Clarkisbred@users.noreply.github.com>
2026-06-23 17:47:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
 window.BREDUCK_CONFIG = {
   SUPABASE_URL : 'https://shnbutvfutvfhakpbvbn.supabase.co',
   SUPABASE_KEY : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNobmJ1dHZmdXR2Zmhha3BidmJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNjIzMjEsImV4cCI6MjA5MjkzODMyMX0.QZYmpeeGWa8F3YddlxbcKkmYwGrRjdQ5iNau6nJpuVQ',
-  GROQ_KEY     : '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',
+  GROQ_KEY     : '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',
   ADMIN_PW     : 'clarkson3?!',
 };
